7adf4860329e816edf91ae41a8b1befad91861f0
Theosaurus-Rex <theosaurus13@gmail.com>
2024-12-26 11:45:21 +1100

    refactor: Use link_to helper in index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 end
